534892e1a404e717accb2624ed486192ef68a01e
Muhammed Yesilkaya <6037070@mborijnland.nl>
2024-04-03 11:38:02 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -18,6 +18,7 @@ $menuItems = array(
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     
+    <title>Gallery</title>
 </head>
 <body>
 
